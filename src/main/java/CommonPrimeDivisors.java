14473b9f724a743a62ab2da89fc1366d137f7e21
Abdullah Sanver <a_sanver@hotmail.com>
2018-10-05 23:39:26 +0100

    gcd method explanation added.

--- src/main/java/CommonPrimeDivisors.java
+++ src/main/java/CommonPrimeDivisors.java
@@ -5,6 +5,11 @@ public class CommonPrimeDivisors {
 
         while (b > 0) {
             c = b;
+            // If b is not the smaller of the two, in the first iteration b = a%b will make b->a
+            // and a = c will make a -> b.
+            // Then in the continuing iterations since a % b is smaller than b, a-> b and b-> a%b and
+            // b will remain the smaller of the two.
+            // Since gcd of a and b is also gcd of a%b and b we can calculate it iteratively until a%b becomes 0.
             b = a % b;
             a = c;
         }
