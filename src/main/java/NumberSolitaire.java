c7aa22ab5bca006ee6094b6ac058150a469756e7
Abdullah Sanver <a_sanver@hotmail.com>
2018-10-16 15:17:09 +0100

    NumberSolitaire added.

--- src/main/java/NumberSolitaire.java
+++ src/main/java/NumberSolitaire.java
@@ -1,2 +1,33 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class NumberSolitaire {
+
+    public static final int MOVE_SIZE = 6;
+
+    public int solution(int[] a) {
+        int i, n = a.length;
+        Deque<Integer> previous = new LinkedList<>();
+        previous.add(a[0]);
+
+        for (i = 1; i < n; i++) {
+            previous.addLast(findMax(previous) + a[i]);
+
+            if (previous.size() == MOVE_SIZE + 1)
+                previous.removeFirst();
+        }
+
+        return previous.getLast();
+    }
+
+    private static int findMax(Deque<Integer> deque) {
+        int max = Integer.MIN_VALUE;
+        if (deque == null || deque.isEmpty())
+            return max;
+
+        for (Integer value : deque)
+            max = Math.max(max, value);
+
+        return max;
+    }
 }
--- src/test/java/NumberSolitaireTest.java
+++ src/test/java/NumberSolitaireTest.java
@@ -1,6 +1,20 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NumberSolitaireTest {
     private NumberSolitaire numberSolitaire = new NumberSolitaire();
+
+    @Test
+    public void should_Return_Sum_When_All_NonNegative() {
+        assertEquals(3, numberSolitaire.solution(new int[]{2, 1}));
+        assertEquals(23, numberSolitaire.solution(new int[]{5, 3, 7, 2, 0, 6}));
+    }
+
+    @Test
+    public void should_Return_1() {
+        assertEquals(1, numberSolitaire.solution(new int[]{2, -1, -3, -1, -2, -6, -4, -3, -2, -1, -2, 1}));
+    }
 }
