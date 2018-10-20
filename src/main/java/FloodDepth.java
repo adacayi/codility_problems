3e2aed33e0c38ae83aae1cc501e085f205863382
Abdullah Sanver <a_sanver@hotmail.com>
2018-10-20 09:37:09 +0100

    FloodDepth simplified.

--- src/main/java/FloodDepth.java
+++ src/main/java/FloodDepth.java
@@ -1,36 +1,19 @@
 public class FloodDepth {
     public int solution(int[] a) {
-        int i, n = a.length, maxDepth = 0, beginHeight = 0, height, depth = 0;
+        int i, n = a.length, maxDepth = 0, beginHeight = 0, lowestHeight = 0, height, depth = 0;
 
-        // Searching for pools with right side higher.
         for (i = 0; i < n; i++) {
             height = a[i];
 
-            if (height < beginHeight) {
-                depth = Math.max(depth, beginHeight - height);
-                continue;
+            if (height < lowestHeight)
+                lowestHeight = height;
+            else if (height < beginHeight)
+                maxDepth = Math.max(maxDepth, height - lowestHeight);
+            else {
+                maxDepth = Math.max(maxDepth, beginHeight - lowestHeight);
+                beginHeight = height;
+                lowestHeight = height;
             }
-
-            maxDepth = Math.max(maxDepth, depth);
-            beginHeight = height;
-            depth = 0;
-        }
-
-        // Searching for pools with left side higher.
-        beginHeight = 0;
-        depth = 0;
-
-        for (i = n - 1; i >= 0; i--) {
-            height = a[i];
-
-            if (height < beginHeight) {
-                depth = Math.max(depth, beginHeight - height);
-                continue;
-            }
-
-            maxDepth = Math.max(maxDepth, depth);
-            beginHeight = height;
-            depth = 0;
         }
 
         return maxDepth;
